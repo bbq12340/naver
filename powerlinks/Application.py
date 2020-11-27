7d2c027b4f18bb53309fe0b6edb9f9156bbe0365
bbq12340 <58031020+bbq12340@users.noreply.github.com>
2020-11-27 18:19:08 +0900

    3rd commit

--- powerlinks/Application.py
+++ powerlinks/Application.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, filedialog
 from threading import Thread
 
 from ScrapeNaverPowerlinks import Scraper
@@ -9,17 +9,16 @@ class Application(tk.Frame):
         super().__init__(master)
         self.master = master
         self.pack()
-        self.query = tk.StringVar()
         self.v = tk.IntVar()
 
         self.create_widgets()
 
     def create_widgets(self):
         self.label_frame = tk.Frame(self)
-        self.search_label = tk.Label(self.label_frame, text="검색어")
-        self.search_label.pack(side=tk.LEFT)
-        self.search_entry = tk.Entry(self.label_frame, textvariable=self.query)
-        self.search_entry.pack(side=tk.LEFT)
+        self.filename_label = tk.Label(self.label_frame, text="")
+        self.filename_label.grid(row=0, column=1)
+        self.request_label = tk.Label(self.label_frame, text='요청 파일명:')
+        self.request_label.grid(row=0, column=0)
         self.label_frame.pack(pady=5, padx=5)
 
         self.btn_frame = tk.Frame(self)
@@ -27,15 +26,25 @@ class Application(tk.Frame):
         self.phone_numbers = tk.Radiobutton(self.btn_frame, text="전화번호", variable=self.v, value=2).pack()
         self.btn_frame.pack(pady=5, padx=5)
 
+        self.file_btn = tk.Button(self, text='파일 탐색', command=self.add_file)
+        self.file_btn.pack(pady=5, padx=5)
         self.search_btn = tk.Button(self, text="검색", command=self.start_process)
         self.search_btn.pack(pady=5, padx=5)
 
         self.progress_frame = tk.Frame(self)
+        self.query_progress = tk.Label(self.progress_frame, text="요청 검색어:")
+        self.query_progress.grid(row=0, column=0)
         self.progress = ttk.Progressbar(self.progress_frame, length=150)
         self.progress_label = tk.Label(self.progress_frame, text="")
-        self.progress.grid(row=0, column=0, padx=5)
-        self.progress_label.grid(row=0, column=1, padx=5)
+        self.progress.grid(row=1, column=0, padx=5)
+        self.progress_label.grid(row=1, column=1, padx=5)
         self.progress_frame.pack(pady=5, padx=5)
+
+    
+    def add_file(self):
+        filename = filedialog.askopenfilename(initialdir='./list', title='파일 탐색', filetypes=[("text files", "*.txt")])
+        self.filename_label.config(text=f"{filename.split('/')[-1]}")
+        self.FILENAME = filename
 
     
     def start_process(self):
@@ -44,20 +53,26 @@ class Application(tk.Frame):
         return
     
     def scrape(self):
-        app = Scraper(self.query.get(), self.progress, self.master)
-        if self.v.get() == 1:
-            self.progress_label.config(text="1/1")
-            df = app.extract_powerlinks()
-        else:
-            self.progress_label.config(text="1/2")
-            df = app.extract_powerlinks()
+        with open(self.FILENAME, 'r', encoding='utf-8-sig') as f:
+            query_list = f.read().splitlines()
+        for query in query_list:
+            self.query_progress.config(text=f"요청 검색어:{query} ({query_list.index(query)+1}/{len(query_list)})")
+            self.master.update_idletasks()
+            app = Scraper(query, self.progress, self.master)
+            if self.v.get() == 1:
+                self.progress_label.config(text="1/1")
+                df = app.extract_powerlinks()
+            else:
+                self.progress_label.config(text="1/2")
+                df = app.extract_powerlinks()
+                self.progress['value'] = 0
+                self.master.update_idletasks()
+                self.progress_label.config(text="2/2")
+                df = app.extract_phone(df)
+            df.to_csv(f'{query}.csv', index=False, encoding='utf-8-sig')
             self.progress['value'] = 0
             self.master.update_idletasks()
-            self.progress_label.config(text="2/2")
-            df = app.extract_phone(df)
-        df.to_csv(f'{self.query.get()}.csv', index=False, encoding='utf-8-sig')
-        self.progress['value'] = 0
-        self.master.update_idletasks()
+
         messagebox.showinfo('info', message="크롤링 완료!")
         return
 
