4992a82ea4e3b5e907cada943dae79c605b3e77e
bbq12340 <58031020+bbq12340@users.noreply.github.com>
2020-11-30 21:22:00 +0900

    Update Scraper.py

--- naverNews/Scraper.py
+++ naverNews/Scraper.py
@@ -1,37 +1,62 @@
-
 import requests, re
 from bs4 import BeautifulSoup
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+
 
 class Scraper():
-    def __init__(self, date):
-        self.API_URL  = "https://news.naver.com/main/list.nhn"
+    def __init__(self):
+        self.API_URL = "https://news.naver.com/main/list.nhn"
         self.headers = {
             'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36"
         }
-        self.date = date
-        self.email_format = re.compile(r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+        self.email_format = re.compile(r"[0-9a-zA-Z\.-]+@[0-9a-zA-Z\.-]+\.[a-zA-Z\.]+")
+        self.reporter_format = re.compile(r'[가-힣]{2,3}\s?기자[\n\s\t]')
     
-    def extract_main_url(self, page):
-        extracted_mails = []
+    def extract_main_url(self, url, page):
+        extracted = []
+        parsed = parse_qs(urlparse.urlparse(url).query)
         params = {
-            "mode": "LSD",
-            "mid": "sec",
-            "sid1": "105",
-            "date": self.date,
+            "mode": parsed['mode'][0],
+            "mid": parsed['mid'][0],
+            "sid1": parsed['sid1'][0],
             "page": page
         }
         r = requests.get(self.API_URL, params=params, headers=self.headers)
-        soup = BeautifulSoup(r.text, 'html.paser')
-        headlines = soup.find('ul', {'class':'type06_headline'}).find_all('li')
-        for head in headlines:
+        if r.status_code != 200:
+            return
+        soup = BeautifulSoup(r.text, 'html.parser')
+        headlines1 = soup.find('ul', {'class':'type06_headline'}).find_all('li')
+        for head in headlines1:
             url = head.find('a')['href']
-            mail = self.extract_reporter(url)
-            extracted_mails.append(mail)
-        return
+            data = self.extract_reporter(url)
+            extracted.append(data)
+        headlines2 = soup.find('ul',{'class':'type06'}).find_all('li')
+        for head in headlines2:
+            url = head.find('a')['href']
+            data = self.extract_reporter(url)
+            extracted.append(data)
+        return extracted
 
     def extract_reporter(self, url):
+        print(url)
         r = requests.get(url, headers=self.headers)
-        soup = BeautifulSoup(r.text, 'html.pasrser')
+        soup = BeautifulSoup(r.text, 'html.parser')
+        article_body = soup.find('div',{'class':'_article_body_contents'})
+        try:
+            email = self.email_format.findall(article_body.text)[-1]
+        except:
+            email = None
+        try:
+            reporter = self.reporter_format.findall(article_body.text)[-1].replace('기자','')
+        except:
+            reporter = None
+        data = {
+            'reporter': reporter,
+            'email': email,
+            'link': url
+        }
+        return data
+
+
         
-        email = self.email_format.search(soup.find('div',{'class':'_article_body_contents'}).text).group(0)
-        return email
