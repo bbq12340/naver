4fa15f2d96e9c182b2a3d93e60cb3084a4c37dc2
bbq12340 <58031020+bbq12340@users.noreply.github.com>
2020-12-31 11:34:43 +0900

    Update NaverMapScraper.py

--- map/version 2.0/NaverMapScraper.py
+++ map/version 2.0/NaverMapScraper.py
@@ -82,6 +82,33 @@ class NaverMapScraper:
         r = requests.get(self.API_URL, params=payload, headers=self.header).json()
         # r.encoding = 'utf-8'
         return r
+    
+    def scrape_restaurants(self, **kwargs):
+        # kwargs = {'order': 배달(delivery), 'postCode': 우편번호, 'url': 홈페이지 }
+        scraped_items  = []
+        i=1 #start
+        n=0 #items
+        while True:
+            r = self.get_restaurants(self.query, i, kwargs['order'])
+            items = r['data']['restaurants']['items']
+            total = int(r['data']['restaurants']['total'])
+            for item in items:
+                data = {
+                    '업체명': item["name"],
+                    '전화번호': item["phone"],
+                    '주소': item["address"],
+                    'id': item["id"]
+                }
+                if kwargs['postCode'] == True:
+                    data['우편번호'] = self.get_postcode(item["address"])
+                if kwargs['url'] == True:
+                    urlList = self.get_summary(item["id"])
+                    for url in urlList:
+                        data[f"{urlList.index(url)}"] = url["url"]
+                df = pd.DataFrame([data], columns=list(data.keys()))
+            i=i+50
+            if n >= total:
+                break
 
     def get_summary(self, id):
         URL=f"https://map.naver.com/v5/api/sites/summary/{id}?lang=ko"
@@ -102,35 +129,16 @@ class NaverMapScraper:
         }
         r = requests.get(URL, params=params, headers=self.header)
         soup = BeautifulSoup(r.text, 'html.parser')
-        section = soup.find("section", {"class": "sc_new ds_address"})
-        p_code = section.find("td", {"class": "tc"}).text
+        try:
+            section = soup.find("div", {"id": "loc-main-section-root"})
+            p_code = section.find("span", {"class": "_49NYn"}).text.strip("우편번호")
+        except:
+            return ""
         return p_code
-
     
-    def scrape_restaurants(self, **kwargs):
-        # kwargs = {'order': 배달(delivery), 'postCode': 우편번호, 'url': 홈페이지 }
-        scraped_items  = []
-        i=1
-        while True:
-            r = self.get_restaurants(self.query, i, kwargs['order'])
-            items = r['data']['restaurants']['items']
-            for item in items:
-                data = {
-                    '업체명': item["name"],
-                    '전화번호': item["phone"],
-                    '주소': item["address"],
-                    'id': item["id"]
-                }
-                if kwargs['postCode'] == True:
-                    data['우편번호'] = self.get_postcode(item["address"])
-                if kwargs['url'] == True:
-                    urlList = self.get_summary(item["id"])
-
-            i=i+1
-    
-    def get_restaurants(self, location, page, **kwargs):
+    def get_restaurants(self, location, start, **kwargs):
         query = "query getRestaurants($input: RestaurantsInput, $isNmap: Boolean!, $isBounds: Boolean!) {↵  restaurants(input: $input) {↵    total↵    items {↵      ...RestaurantItemFields↵      easyOrder {↵        easyOrderId↵        easyOrderCid↵        businessHours {↵          weekday {↵            start↵            end↵            __typename↵          }↵          weekend {↵            start↵            end↵            __typename↵          }↵          __typename↵        }↵        __typename↵      }↵      baemin {↵        businessHours {↵          deliveryTime {↵            start↵            end↵            __typename↵          }↵          closeDate {↵            start↵            end↵            __typename↵          }↵          temporaryCloseDate {↵            start↵            end↵            __typename↵          }↵          __typename↵        }↵        __typename↵      }↵      yogiyo {↵        businessHours {↵          actualDeliveryTime {↵            start↵            end↵            __typename↵          }↵          bizHours {↵            start↵            end↵            __typename↵          }↵          __typename↵        }↵        __typename↵      }↵      __typename↵    }↵    nlu {↵      ...NluFields↵      __typename↵    }↵    brand {↵      name↵      isBrand↵      type↵      menus {↵        order↵        id↵        images {↵          url↵          desc↵          __typename↵        }↵        name↵        desc↵        price↵        isRepresentative↵        detailUrl↵        orderType↵        catalogId↵        source↵        menuId↵        nutrients↵        allergies↵        __typename↵      }↵      __typename↵    }↵    optionsForMap @include(if: $isBounds) {↵      maxZoom↵      minZoom↵      includeMyLocation↵      maxIncludePoiCount↵      center↵      spotId↵      __typename↵    }↵    __typename↵  }↵}↵↵fragment RestaurantItemFields on RestaurantSummary {↵  id↵  dbType↵  name↵  businessCategory↵  category↵  description↵  hasBooking↵  hasNPay↵  x↵  y↵  distance↵  imageUrl↵  imageUrls↵  imageCount↵  phone↵  virtualPhone↵  routeUrl↵  streetPanorama {↵    id↵    pan↵    tilt↵    lat↵    lon↵    __typename↵  }↵  roadAddress↵  address↵  commonAddress↵  blogCafeReviewCount↵  bookingReviewCount↵  totalReviewCount↵  bookingReviewScore↵  bookingUrl↵  bookingHubUrl↵  bookingHubButtonName↵  bookingBusinessId↵  talktalkUrl↵  options↵  promotionTitle↵  agencyId↵  businessHours↵  microReview↵  tags↵  priceCategory↵  broadcastInfo {↵    program↵    date↵    menu↵    __typename↵  }↵  michelinGuide {↵    year↵    star↵    comment↵    url↵    hasGrade↵    isBib↵    alternateText↵    __typename↵  }↵  broadcasts {↵    program↵    menu↵    episode↵    broadcast_date↵    __typename↵  }↵  tvcastId↵  naverBookingCategory↵  saveCount↵  uniqueBroadcasts↵  isDelivery↵  isCvsDelivery↵  markerLabel @include(if: $isNmap) {↵    text↵    style↵    __typename↵  }↵  imageMarker @include(if: $isNmap) {↵    marker↵    markerSelected↵    __typename↵  }↵  isTableOrder↵  isPreOrder↵  isTakeOut↵  bookingDisplayName↵  bookingVisitId↵  bookingPickupId↵  popularMenuImages {↵    name↵    price↵    bookingCount↵    menuUrl↵    menuListUrl↵    imageUrl↵    isPopular↵    usePanoramaImage↵    __typename↵  }↵  visitorReviewCount↵  visitorReviewScore↵  detailCid {↵    c0↵    c1↵    c2↵    c3↵    __typename↵  }↵  streetPanorama {↵    id↵    pan↵    tilt↵    lat↵    lon↵    __typename↵  }↵  __typename↵}↵↵fragment NluFields on Nlu {↵  queryType↵  user {↵    gender↵    __typename↵  }↵  queryResult {↵    ptn0↵    ptn1↵    region↵    spot↵    tradeName↵    service↵    selectedRegion {↵      name↵      index↵      x↵      y↵      __typename↵    }↵    selectedRegionIndex↵    otherRegions {↵      name↵      index↵      __typename↵    }↵    property↵    keyword↵    queryType↵    nluQuery↵    businessType↵    cid↵    branch↵    franchise↵    titleKeyword↵    location {↵      x↵      y↵      default↵      longitude↵      latitude↵      dong↵      si↵      __typename↵    }↵    noRegionQuery↵    priority↵    showLocationBarFlag↵    themeId↵    filterBooking↵    repRegion↵    repSpot↵    dbQuery {↵      isDefault↵      name↵      type↵      getType↵      useFilter↵      hasComponents↵      __typename↵    }↵    type↵    category↵    menu↵    __typename↵  }↵  __typename↵}↵"
-        if kwargs['order'] == 'delivery':
+        if kwargs['order'] == True:
             order = "배달"
         else:
             order = "off"
@@ -144,7 +152,7 @@ class NaverMapScraper:
                     "isNmap": False,
                     "order": order,
                     "query": location,
-                    "start": page,
+                    "start": start,
                 },
                 "isBounds": True,
                 "isNmap": False
