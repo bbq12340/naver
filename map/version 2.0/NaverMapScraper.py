336c3b11a3ee09a5a9342e18583d87813410f95a
David Jang <58031020+bbq12340@users.noreply.github.com>
2021-04-19 20:16:27 +0900

    Update NaverMapScraper.py

--- map/version 2.0/NaverMapScraper.py
+++ map/version 2.0/NaverMapScraper.py
@@ -11,6 +11,9 @@ class NaverMapScraper:
         self.graphql_url = 'https://pcmap-api.place.naver.com/place/graphql'
 
         self.header = {
+            'authority': 'map.naver.com',
+            'method': 'GET',
+            'scheme': 'https',
             'referer': 'https://map.naver.com/',
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
         }
